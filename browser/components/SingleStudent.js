d6bd5a92ab65e4c158de0b2ff8e452bb65840942
Noelle Laureano <nrlaureano@gmail.com>
2020-10-20 18:16:51 -0400

    Fixes bug with student without tests in the SingleStudent component

--- browser/components/SingleStudent.js
+++ browser/components/SingleStudent.js
@@ -7,31 +7,39 @@ const avgGrade = (tests) => {
 };
 
 const SingleStudent = (props) => {
-  console.log('ppp', props);
+  const { student } = props;
+  const hasTests = !!student.tests.length;
+
   return (
     <div>
-      <h3>{props.student.fullName}</h3>
-      <h3>Average grade: {avgGrade(props.student.tests)}%</h3>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Subject</th>
-              <th>Grade</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.student.tests.map((test) => {
-              return (
-                <tr key={test.id}>
-                  <td>{test.subject}</td>
-                  <td>{test.grade}%</td>
+      <h3>{student.fullName}</h3>
+      {hasTests ? (
+        <React.Fragment>
+          <h3>Average grade: {avgGrade(student.tests)}%</h3>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Subject</th>
+                  <th>Grade</th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+              </thead>
+              <tbody>
+                {student.tests.map((test) => {
+                  return (
+                    <tr key={test.id}>
+                      <td>{test.subject}</td>
+                      <td>{test.grade}%</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </React.Fragment>
+      ) : (
+        <h4>No tests on record.</h4>
+      )}
     </div>
   );
 };
