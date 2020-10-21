712459cd68f55e07fb48cbb6a122da7ef6c52231
Noelle Laureano <nrlaureano@gmail.com>
2020-10-21 17:30:36 -0400

    Updates Student beforeCreate method syntax

--- server/db/models/students.js
+++ server/db/models/students.js
@@ -3,44 +3,38 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define(
-  'student',
-  {
-    firstName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        isEmail: true
-      }
-    },
-    fullName: {
-      type: Sequelize.VIRTUAL,
-      get() {
-        return `${this.firstName} ${this.lastName}`;
-      }
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
     }
   },
-  {
-    hooks: {
-      beforeCreate: (student) => {
-        student.firstName = `${student.firstName[0].toUpperCase()}${student.firstName.slice(
-          1
-        )}`;
-        student.lastName = `${student.lastName[0].toUpperCase()}${student.lastName.slice(
-          1
-        )}`;
-      }
+  fullName: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      return `${this.firstName} ${this.lastName}`;
     }
   }
-);
+});
+
+Student.beforeCreate((student) => {
+  student.firstName = `${student.firstName[0].toUpperCase()}${student.firstName.slice(
+    1
+  )}`;
+  student.lastName = `${student.lastName[0].toUpperCase()}${student.lastName.slice(
+    1
+  )}`;
+});
 
 Student.prototype.initials = function () {
   return `${this.firstName[0]} ${this.lastName[0]}`;
