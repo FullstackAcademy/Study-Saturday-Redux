639f47c51f819938f73a961dabff07f247d041cd
Jess B <jessica.bracht@gracehopper.com>
2020-12-03 13:57:23 -0600

    partial cycle-0 solution

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # Study Saturdays: Week 3 - React-Redux
 
 ## **Objective:** Build a Redux store to display all students in our app
-- [ ] **Cycle 0:** Create the store ⭐️
-- [ ] **Cycle 1:** `connect` your React components to your Redux store
+- [x] **Cycle 0:** Build the store
+- [ ] **Cycle 1:** `connect` your React components to your Redux store ⭐️
 - [ ] **Cycle 2:** Display the dummy data
 - [ ] **Cycle 3:** Write the thunk
 - [ ] **Cycle 4 (Bonus):** Delete a student
 
 ## Steps
 
-- [ ] Build out the Redux store
-  - See the dummy data in `components/Main.js` for an example of how the data will be structured
-- [ ] Test your Redux store functionality
-  - Dispatch a few actions to your reducer, then check the logs in your console. Is it doing what you expect?
-- [ ] Once done and tested, `git checkout cycle-1`
+- [ ]
+- [ ] Once done and tested, `git checkout cycle-2`
--- client/redux/store.js
+++ client/redux/store.js
@@ -2,15 +2,26 @@ import {createStore, applyMiddleware} from 'redux';
 import loggerMiddleware from 'redux-logger';
 
 // ACTION TYPES go here:
+const GOT_STUDENTS = 'GOT_STUDENTS';
 
 
 // ACTION CREATORS go here:
+const gotStudents = (students) => ({
+  type: GOT_STUDENTS,
+  students
+});
 
 
-const initialState = {};
+const initialState = {
+  students: []
+};
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case GOT_STUDENTS:
+      return {
+        students: action.students
+      }
     default:
       return state;
   }
@@ -19,8 +30,7 @@ const reducer = (state = initialState, action) => {
 const store = createStore(reducer, applyMiddleware(loggerMiddleware));
 
 // dispatch a few actions to test your store's functionality:
-store.dispatch(/* call your action creator here */)
+// store.dispatch(gotStudents(['sally', 'bob', 'sue']))
 
-// ^ you can see the logs above in your console, thanks to redux-logger!
 
 export default store;
