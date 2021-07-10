5d5eedf577217cfff04a06afdb786e880574ea11
Traci <tas1013@gmail.com>
2021-07-10 10:32:49 -0500

    Solution for Cylce-0

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,25 +1,38 @@
-import {createStore, applyMiddleware} from 'redux';
+import axios from 'axios';
+import { createStore, applyMiddleware } from 'redux';
 import loggerMiddleware from 'redux-logger';
 
 // ACTION TYPES go here:
-
+const GET_STUDENTS = 'GET_STUDENTS'
 
 // ACTION CREATORS go here:
-
+const getStudents = (students) => ({
+  type: GET_STUDENTS,
+  students
+})
 
 // THUNK CREATORS go here:
+const fetchStudents = () => async (dispatch) => {
+  const { data } = await axios.get('/api/students')
+  dispatch(getStudents(data))
+}
 
-
-const initialState = {};
+const initialState = {
+  students: []
+};
 
 const reducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
+    case GET_STUDENTS:
+      return {
+        students: action.students
+      }
     default:
       return state;
   }
 }
 
 const store = createStore(reducer, applyMiddleware(loggerMiddleware));
-store.dispatch({type: 'HELLO_WORLD'})
+store.dispatch({ type: 'HELLO_WORLD' })
 
 export default store;
