51509f7a22bd1bcc85967d530f4cf6b2b1fb5cd7
Torie <vkim20@gmail.com>
2021-10-25 21:19:30 -0400

    refactor: SingleStudent to functional component

--- client/components/SingleStudent.js
+++ client/components/SingleStudent.js
@@ -1,53 +1,47 @@
-import React from 'react';
+import React from 'react'
 
 const avgGrade = (tests) => {
   return Math.round(
     tests.map((test) => test.grade).reduce((x, y) => x + y) / tests.length
-  );
-};
+  )
+}
 
-class SingleStudent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function SingleStudent(props) {
+  const { student } = props
+  const hasTests = student.tests.length
 
-  render() {
-    const { student } = this.props;
-    const hasTests = student.tests.length;
+  return (
+    <div>
+      <h3>Detail: {student.fullName}</h3>
+      {hasTests ? (
+        <React.Fragment>
+          <h3>Average grade: {avgGrade(student.tests)}%</h3>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Subject</th>
+                  <th>Grade</th>
+                </tr>
+              </thead>
+              <tbody>
+                {student.tests.map((test) => {
+                  return (
+                    <tr key={test.id}>
+                      <td>{test.subject}</td>
+                      <td>{test.grade}%</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </React.Fragment>
+      ) : (
+        <h4>No tests on record.</h4>
+      )}
+    </div>
+  )
+}
 
-    return (
-      <div>
-        <h3>Detail: {student.fullName}</h3>
-        {hasTests ? (
-          <React.Fragment>
-            <h3>Average grade: {avgGrade(student.tests)}%</h3>
-            <div>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Subject</th>
-                    <th>Grade</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {student.tests.map((test) => {
-                    return (
-                      <tr key={test.id}>
-                        <td>{test.subject}</td>
-                        <td>{test.grade}%</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </React.Fragment>
-        ) : (
-          <h4>No tests on record.</h4>
-        )}
-      </div>
-    );
-  }
-};
-
-export default SingleStudent;
+export default SingleStudent
