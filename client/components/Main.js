a2973b9ed7cd7f4f8c7bef7bab1d5c9b1f745cc7
Torie <vkim20@gmail.com>
2021-11-30 19:13:19 -0500

    refactor: Main, SingleStudent, StudentList to functional component

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import {HashRouter as Router, Switch, Route} from 'react-router-dom'
-import StudentList from './StudentList.js';
-import SingleStudent from './SingleStudent';
+import React from 'react'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import StudentList from './StudentList.js'
+import SingleStudent from './SingleStudent'
 
-export default class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <h1>Student Dashboard</h1>
-        </div>
-        <Switch>
-          <Route exact path="/" component={StudentList} />
-        </Switch>
-      </Router>
-    );
-  }
+const Main = (props) => {
+  return (
+    <Router>
+      <div>
+        <h1>Student Dashboard</h1>
+      </div>
+      <Switch>
+        <Route exact path="/" component={StudentList} />
+      </Switch>
+    </Router>
+  )
 }
+
+export default Main
